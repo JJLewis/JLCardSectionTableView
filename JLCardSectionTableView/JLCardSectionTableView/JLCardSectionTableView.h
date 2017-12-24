7618845027de478f18e2e602241b5201cc06bf23
Jordan Joseph Lewis <dev_jordanlewis@icloud.com>
2017-12-25 00:49:39 +1100

    setup project

---
+++ JLCardSectionTableView/JLCardSectionTableView/JLCardSectionTableView.h
@@ -0,0 +1,19 @@
+//
+//  JLCardSectionTableView.h
+//  JLCardSectionTableView
+//
+//  Created by Jordan Lewis on 25/12/17.
+//  Copyright © 2017 Jordan Lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JLCardSectionTableView.
+FOUNDATION_EXPORT double JLCardSectionTableViewVersionNumber;
+
+//! Project version string for JLCardSectionTableView.
+FOUNDATION_EXPORT const unsigned char JLCardSectionTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JLCardSectionTableView/PublicHeader.h>
+
+
