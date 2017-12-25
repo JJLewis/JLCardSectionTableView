965377759a3ccd360e59dbbfea6206d9bfc6ff6d
Jordan Joseph Lewis <dev_jordanlewis@icloud.com>
2017-12-26 10:28:45 +1100

    add views to cell xibs

---
+++ JLCardSectionTableView/JLCardSectionTableView/Generics/Rounded Shadow/JLRoundedShadowView.swift
@@ -0,0 +1,21 @@
+//
+//  JLRoundedShadowView.swift
+//  JLCardSectionTableView
+//
+//  Created by Jordan Lewis on 25/12/17.
+//  Copyright © 2017 Jordan Lewis. All rights reserved.
+//
+
+import UIKit
+
+class JLRoundedShadowView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
