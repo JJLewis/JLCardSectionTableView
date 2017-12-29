db9058e34b924e0724d350d0a80698bbebc1f805
Jordan Joseph Lewis <dev_jordanlewis@icloud.com>
2017-12-29 18:08:49 +1100

    create single row section

---
+++ JLCardSectionTableView/JLCardSectionTableView/Cells/Single Row/JLCSSingleRowSectionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  JLCSSingleRowSectionTableViewCell.swift
+//  JLCardSectionTableView
+//
+//  Created by Jordan Lewis on 29/12/17.
+//  Copyright © 2017 Jordan Lewis. All rights reserved.
+//
+
+import UIKit
+
+class JLCSSingleRowSectionTableViewCell: JLCSTableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
